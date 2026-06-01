302eff9ec789b5c6f33477a4bab910e9c087d7be
nikitink1580-ux <nikitin.k1580@gmail.com>
2026-06-01 11:17:03 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,20 +6,25 @@ const levelEl = document.getElementById("level");
 const img = document.getElementById("character");
 
 const stages = [
-"images/girl1.jpg.png",
-"images/girl2.jpg.png",
-"images/girl3.jpg.png",
-"images/girl4.jpg.png",
-"images/girl5.jpg.png",
-"images/girl6.jpg.png"
+    "images/girl1.jpg.png",
+    "images/girl2.jpg.png",
+    "images/girl3.jpg.png",
+    "images/girl4.jpg.png",
+    "images/girl5.jpg.png",
+    "images/girl6.jpg.png"
 ];
+
+img.src = stages[0];
+
 document.getElementById("clickBtn").addEventListener("click", () => {
+
     points++;
     pointsEl.textContent = points;
 
     const newLevel = Math.floor(points / 20) + 1;
 
     if (newLevel > level) {
+
         level = newLevel;
         levelEl.textContent = level;
 
