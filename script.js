a41af5d69273eae0fb01579223074b4544ed6975
nikitink1580-ux <nikitin.k1580@gmail.com>
2026-06-01 11:10:10 +0300

    опля

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const levelEl = document.getElementById("level");
 const img = document.getElementById("character");
 
 const stages = [
-"https://i.pinimg.com/originals/6a/f1/e8/6af1e8bf3596f76fc19ab6df16858077.jpg?nii=t",
-"https://i.pinimg.com/736x/3a/26/54/3a2654ce96a7845663f12f86a0514106.jpg",
-"https://i.pinimg.com/originals/2a/b1/82/2ab18240c7c96241d3cc62e88e5c5505.webp",
-"https://i.pinimg.com/736x/25/43/38/2543384165ea19f0e221577f522db282.jpg",
-"https://a.lmcdn.ru/img600x866/X/D/XD001XW044ZN_31857973_2_v4.jpg"
+"images/girl1.jpg",
+"images/girl2.jpg",
+"images/girl3.jpg",
+"images/girl4.jpg",
+"images/girl5.jpg",
+"images/girl6.jpg"
 ];
-
 document.getElementById("clickBtn").addEventListener("click", () => {
     points++;
     pointsEl.textContent = points;
